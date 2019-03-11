010ad593dca10594f7797ae8c33f9abf79d4c200
Stephanie Purple <spurple133@gmail.com>
2019-03-11 22:17:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli,name)
 katz_deli.push(name)
-puts "Welcome, #{name}. You are number #{(katz_deli.index+1)(name)} in line."
+puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
 end
 
 
