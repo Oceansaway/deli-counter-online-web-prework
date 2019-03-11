9309aa40eac940aa63096ad7b735a99930ee16b8
Stephanie Purple <spurple133@gmail.com>
2019-03-11 21:37:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,15 @@
-def line (katz_deli)
+def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    #puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-	#end
-    message= "The line is currently "
-    katz_deli.each_with_index do |value, index|
+    message="The line is currently:"
+
+     katz_deli.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
-end
-puts "#{message}"
-end
-end
+    end
+
+     puts "#{message}"
+  end
+
+
+ end
