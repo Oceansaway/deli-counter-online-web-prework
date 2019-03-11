32411745980309da2427d955bb37e4e7a62ee669
Stephanie Purple <spurple133@gmail.com>
2019-03-11 22:52:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-  if katz_deli=[]
+  if katz_deli.size==0
   puts "There is nobody waiting to be served!"
 else
   puts "Currently serving #{katz_deli.first}."
