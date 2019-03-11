b3822f6a0036b5e4c267cdd2b63a8f3ca30658c0
Stephanie Purple <spurple133@gmail.com>
2019-03-11 22:45:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,4 +17,13 @@ katz_deli.push(name)
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+def now_serving(katz_deli)
+  if katz_deli==0
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{katz_deli}."
+  katz_deli
+end
+end
+
 #katz_deli.index+1(name)
